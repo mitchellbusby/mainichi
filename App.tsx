a18d017b24ced78b5d6270d0390ecdd3cb49c6cb
Mitchell Busby <mitchbusby@gmail.com>
2019-10-29 13:58:53 +1100

    Partway working lol

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+const App = () => {
+    return (
+        <div>
+            <div>
+                <h1 className="h1">Today</h1>
+            </div>
+        </div>
+    )
+};
+
+export {App};
---
+++ index.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import {render} from "react-dom";
+import {App} from "./App";
+import "./index.scss";
+
+const root = document.getElementById("root");
+
+render(
+    <App />,
+    root
+);
