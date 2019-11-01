b605bed24592a7b47bad7ad392ff6a8007243b29
Mitchell Busby <mitchbusby@gmail.com>
2019-11-01 11:28:21 +1100

    Fix

--- index.tsx
+++ index.tsx
@@ -2,9 +2,6 @@ import * as React from "react";
 import {render} from "react-dom";
 import {App} from "./client/App";
 import "./client/index.scss";
-// import * as dotenv from "dotenv";
-
-// dotenv.config();
 
 const root = document.getElementById("root");
 
