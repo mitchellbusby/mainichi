19a13ed8aa3ceb623fa59500a6775af1c8cb7763
Mitchell Busby <mitchbusby@gmail.com>
2019-11-02 14:46:52 +1100

    Uses cache for localdev

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 /**
  * Server that acts like my netlify deploy 🤷
+ * but with filesystem caching :D
  */
 
 require("dotenv").config();
@@ -7,6 +8,12 @@ const Bundler = require("parcel-bundler");
 const express = require("express");
 const tfnsw = require('./server/TfNsw');
 const weather = require('./server/Weather');
+const flatCache = require('flat-cache');
+
+const cache = flatCache.load('mainichi');
+const CacheKeys = {
+  Weather: 'weather'
+}
 
 let bundler = new Bundler("index.html");
 let app = express();
@@ -17,9 +24,22 @@ app.get('/.netlify/functions/buses', async (req, res) => {
 });
 
 app.get("/.netlify/functions/weather", async (req, res) => {
+  const cachedResponse = cache.getKey(CacheKeys.Weather);
+  if (cachedResponse) {
+    console.log("Used cache result for weather");
+    res.send(cachedResponse);
+    return;
+  }
+
   const response = await weather.getWeatherForecast();
+
+  cache.setKey(CacheKeys.Weather, response);
   res.send(response);
 });
+
+app.get('/clearcache', () => {
+  cache.removeKey(CacheKeys.Weather);
+})
 
 app.use(bundler.middleware());
 
